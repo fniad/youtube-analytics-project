b75111c6f4317c01322c4d05715d94bf07c6c631
fniad <artkamproject@gmail.com>
2023-06-09 20:21:49 +0300

    hw 6: add except for invalid id for video

--- src/video.py
+++ src/video.py
@@ -7,10 +7,10 @@ class Video:
     """Класс видео"""
     def __init__(self, video_id: str) -> None:
         self._video_id = video_id
-        self.title_video = ''
-        self.url_video = ''
-        self.total_views = 0
-        self.total_like = 0
+        self.title_video = None
+        self.url_video = None
+        self.total_views = None
+        self.total_like = None
         # подтягиваем функцией значения, через API
         self.set_initialization()
 
@@ -25,14 +25,18 @@ class Video:
         return self._video_id
 
     def set_initialization(self):
-        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                               id=self._video_id
-                                               ).execute()
-        # printj(video_response)
-        self.title_video: str = video_response['items'][0]['snippet']['title']
-        self.url_video: str = 'https://www.youtube.com/watch?v=' + self._video_id
-        self.total_views: int = video_response['items'][0]['statistics']['viewCount']
-        self.total_like: int = video_response['items'][0]['statistics']['likeCount']
+        try:
+            video_response = youtube.videos().list(part='snippet,statistics',
+                                                   id=self._video_id
+                                                   ).execute()
+
+            self.title_video: str = video_response['items'][0]['snippet']['title']
+        except IndexError:
+            print('Неверный id для видео.')
+        else:
+            self.url_video: str = 'https://www.youtube.com/watch?v=' + self._video_id
+            self.total_views: int = int(video_response['items'][0]['statistics']['viewCount'])
+            self.total_like: int = int(video_response['items'][0]['statistics']['likeCount'])
 
 
 class PLVideo(Video):
